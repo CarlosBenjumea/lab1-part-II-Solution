74d8a8002585c00bda26eb220b4cd06667b771f5
carlos <cabene05@gmail.com>
2026-02-13 11:37:13 +0100

    Primera tarea de round 4

--- billing/api/filters.py
+++ billing/api/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from ..models import Invoice
+from ..models import Invoice, Provider
 
 
 class InvoiceFilter(django_filters.FilterSet):
@@ -9,3 +9,19 @@ class InvoiceFilter(django_filters.FilterSet):
     class Meta:
         model = Invoice
         fields = ["invoice_no", "issued_on"]
+
+
+class ProviderFilter(django_filters.FilterSet):
+    has_barrels_to_bill = django_filters.BooleanFilter(
+        method="filter_has_barrels_to_bill"
+    )
+
+    def filter_has_barrels_to_bill(self, queryset, name, value):
+        if not value:
+            return queryset
+
+        return queryset.filter(barrels__billed=False).distinct()
+
+    class Meta:
+        model = Provider
+        fields = ["has_barrels_to_bill"]
